7a128659161196bd2faf977e694b18c5ca967cf1
Jelle <jellepelle214@gmail.com>
2026-09-21 15:31:05 +0200

    complete

--- start.py
+++ start.py
@@ -17,6 +17,7 @@
 import pygame
 from deck import Deck
 import sys
+import json
 
 class Game:
     def __init__(self):
@@ -27,12 +28,22 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.card_front = self.load_image("card_fronts.png")
+        self.score = 0
+        self.new_card = False
+        
+        try:
+            with open("highscore.json", "r") as file:
+                self.highscore = json.load(file)["highscore"]
+        except FileNotFoundError:
+            self.highscore = 0
+        self.save = False
+        self.out_of_cards = False
         
         self.deck = Deck()
         self.deck.shuffle()
-        self.current_card = self.deck.draw()
+        self.old_card = self.deck.draw()
         
+        self.card_front = self.load_image("card_fronts.png")
         self.background = self.load_image("background.png")
 
 
@@ -52,6 +63,13 @@ class Game:
         left = 3 + rank * 71
         top = 3 + suit * 97
         return (left, top, 71, 97)  # Return the rectangle area for the card front
+    
+    
+    def compare(self, bet):
+        if bet == "higher" and self.deck.get_rank(self.new_card) > self.deck.get_rank(self.old_card):
+            self.score += 1
+        elif bet == "lower" and self.deck.get_rank(self.new_card) < self.deck.get_rank(self.old_card):
+            self.score += 1
 
 
     def run(self):
@@ -60,14 +78,37 @@ class Game:
             self.display.fill((0, 0, 0, 0))  # Clear the screen with black
             self.display.blit(self.background, (0, 0))
 
-            self.display.blit(self.card_front, area=self.getCardFront(self.deck.get_suit(self.current_card), self.deck.get_rank(self.current_card)))  # Draw the card front
+            # Display old card left
+            self.display.blit(self.card_front, dest=(0,0), area=self.getCardFront(self.deck.get_suit(self.old_card), self.deck.get_rank(self.old_card)))
+            # Display new card right
+            if self.new_card:
+                self.display.blit(self.card_front, dest=(320 - 68, 0), area=self.getCardFront(self.deck.get_suit(self.new_card), self.deck.get_rank(self.new_card)))
+            
             width = self.display.get_width()
             height = self.display.get_height()
+            
             #Higher button
-            pygame.draw.rect(self.display, (100,100,100), [width / 2 - 60, height / 2 + 50, 50, 50])
+            higher_button = pygame.Rect(width / 2 - 50, height / 2 + 50, 50, 50)
+            pygame.draw.rect(self.display, (100, 255, 100), higher_button)
             font = pygame.font.SysFont('Corbel', 15)
-            self.display.blit(font.render('Higher', True, (255, 255, 255)), (width / 2 - 60, height / 2 + 65))
+            self.display.blit(font.render('Higher', True, (1, 1, 1)), (width / 2 - 47, height / 2 + 65))
+            
             #Lower button
+            lower_button = pygame.Rect(width / 2 + 5, height / 2 + 50, 50, 50)
+            pygame.draw.rect(self.display, (255, 100, 100), lower_button)
+            self.display.blit(font.render('Lower', True, (1, 1, 1)), (width / 2 + 11, height / 2 + 65))
+            
+            #Score display
+            self.display.blit(font.render(f'Score: {self.score}', True, (1, 1, 1)), (width / 2 - 25, 20))
+            
+            #Highscore display
+            self.display.blit(font.render(f'Highscore: {self.highscore}', True, (1, 1, 1)), (width / 2 - 53, 40))
+            
+            #Restart button
+            if self.out_of_cards:
+                restart_button = pygame.Rect(width / 2 - 25, 60, 50, 50)
+                pygame.draw.rect(self.display, (100, 100, 255), restart_button)
+                self.display.blit(font.render('Restart', True, (1, 1, 1)), (width / 2 - 21, 75))
             
 
             for event in pygame.event.get():
@@ -77,12 +118,48 @@ class Game:
                     sys.exit()
                 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    pass
+                    mouse_x = event.pos[0] * width / self.screen.get_width()
+                    mouse_y = event.pos[1] * height / self.screen.get_height()
+                    if higher_button.collidepoint(mouse_x, mouse_y) and not self.out_of_cards:
+                        if self.new_card:
+                            self.old_card = self.new_card
+                        try:
+                            self.new_card = self.deck.draw()
+                            self.compare("higher")
+                        except IndexError:
+                            if self.highscore < self.score:
+                                with open("highscore.json", "w") as file:
+                                    file.write(json.dumps({"highscore": self.score}))
+                            self.out_of_cards = True
+
+                    if lower_button.collidepoint(mouse_x, mouse_y) and not self.out_of_cards:
+                        if self.new_card:
+                            self.old_card = self.new_card
+                        try:
+                            self.new_card = self.deck.draw()
+                            self.compare("lower")
+                        except IndexError:
+                            if self.highscore < self.score:
+                                with open("highscore.json", "w") as file:
+                                    file.write(json.dumps({"highscore": self.score}))
+                            self.out_of_cards = True
+                    
+                    try:
+                        if restart_button.collidepoint(mouse_x, mouse_y):
+                            self.out_of_cards = False
+                            self.score = 0
+                            self.new_card = False
+                            self.deck = Deck()
+                            self.deck.shuffle()
+                            self.old_card = self.deck.draw()
+                            self.highscore = max(self.highscore, self.score)
+                    except UnboundLocalError:
+                        pass
             
             
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))  # Scale the display to the screen size
             pygame.display.update()  # Update the display
-            self.clock.tick(30)  # Limit to 60 frames per second
+            self.clock.tick(30)  # Limit to 30 frames per second
 
 
 Game().run()
